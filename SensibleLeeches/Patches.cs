889d3211e52356d7861f41b300b308bb10dfe522
Majura-fi <12672127+Majura-fi@users.noreply.github.com>
2024-09-23 21:28:08 +0300

    SensibleLeeches mod

---
+++ SensibleLeeches/Patches.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace SensibleLeeches;
+
+[HarmonyPatch(typeof(PlayerInjuryModule), nameof(PlayerInjuryModule.CheckLeeches))]
+internal class PlayerInjuryModule_CheckLeeches
+{
+    static void Prefix(PlayerInjuryModule __instance)
+    {
+        FieldInfo? f = __instance.GetType().GetField("m_LeechChanceOutsideOfWater", BindingFlags.NonPublic | BindingFlags.Instance);
+        f?.SetValue(__instance, 0.05f);
+
+        if (f == null)
+        {
+            Plugin.ModEntry?.Logger.Warning("m_LeechChanceOutsideOfWater not found.");
+        }
+    }
+}
---
+++ SensibleLeeches/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using static UnityModManagerNet.UnityModManager;
+
+namespace SensibleLeeches;
+
+public static class Plugin
+{
+    private static Harmony? harmony;
+    public static ModEntry? ModEntry { get; private set; }
+    public static bool Enabled { get; private set; }
+
+    public static bool Load(ModEntry mod)
+    {
+        harmony = new Harmony(mod.Info.Id);
+
+        ModEntry = mod;
+        ModEntry.OnToggle = OnToggle;
+        ModEntry.Logger.Log("SensibleLeeches loaded.");
+        return true;
+    }
+
+    private static bool OnToggle(ModEntry entry, bool enable)
+    {
+        if (enable)
+        {
+            harmony!.PatchAll(Assembly.GetExecutingAssembly());
+            entry.Logger.Log("SensibleLeeches enabled.");
+        }
+        else
+        {
+            harmony!.UnpatchAll(ModEntry!.Info.Id);
+            entry.Logger.Log("SensibleLeeches disabled.");
+        }
+
+        Enabled = enable;
+        return true;
+    }
+}
