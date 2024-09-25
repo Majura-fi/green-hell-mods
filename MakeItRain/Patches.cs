5f989648e85221c878e7dfe1f31aa9da73ffddfa
Majura-fi <12672127+Majura-fi@users.noreply.github.com>
2024-09-25 22:19:15 +0300

    MakeItRain mod

---
+++ MakeItRain/Patches.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace MakeItRain;
+
+[HarmonyPatch]
+internal class RainManager_UpdateAreaDensity
+{
+    static MethodInfo TargetMethod()
+    {
+        return typeof(RainManager).GetMethod("UpdateAreaDensity", BindingFlags.NonPublic | BindingFlags.Instance);
+    }
+
+    static void Postfix()
+    {
+        RainManager __instance = RainManager.Get();
+        MethodInfo m = __instance.GetType().GetMethod("GetAreaDensity", BindingFlags.NonPublic | BindingFlags.Instance);
+        AreaDensity result = (AreaDensity)m.Invoke(__instance, null);
+
+        // AreaDensity.Low and AreaDensity.None means 0% chance.
+        // Let's bump it to 25% so we get a little bit of rain.
+        if (result == AreaDensity.Low)
+        {
+            FieldInfo f = __instance.GetType().GetField("m_WantedAreaDensity", BindingFlags.NonPublic | BindingFlags.Instance);
+            f.SetValue(__instance, 0.25f);
+        }
+    }
+}
